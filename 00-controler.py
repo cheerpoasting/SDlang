a7942f08004eaa586eba96eece7cfbb58a63ce65
cheerpoasting <130847038+cheerpoasting@users.noreply.github.com>
2025-08-10 20:28:08 -0500

    created main function
    created a basic "hello world" type program

---
+++ 00-controler.py
@@ -0,0 +1,24 @@
+############################################################################
+#                                                                          #
+#      Self-Documenting Programming Language with a focus on Business      #
+#                                                                          #
+# Version Number: 00.00.01-alpha                                           #
+#                                                                          #
+# Last Updated: 08 AUG 2025                                                #
+#                                                                          #
+# Program Purpose: This program will open a source file, and write its     #
+#   contents directly to an output file, without transforming it in        #
+#   any way.                                                               #
+#                                                                          #
+############################################################################
+
+# TEMPLATE
+# Open input file
+# Transform contents (later)
+# Write Contents to output file
+
+def main():
+    print("program ran")
+
+if __name__ == "__main__":
+    main()
